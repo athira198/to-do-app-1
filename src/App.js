1b22e1c7ee1542067fb6957ca91d5f8183a322db
Athira.p <athirapaluthodi@gmail.com>
2022-02-07 14:48:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,65 @@
-import React, { Component } from 'react';
-import"./App.css"
+import React from 'react';
+import {useState} from 'react'
+import './App.css';
+
+function App() {
+  const [toDos,setToDos]=useState([])
+  const [toDo,setToDo] = useState('')
+  return ( 
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Start your day 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setToDo(e.target.value)}  type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDos([...toDos,{id:Date.now() ,text: toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+      { toDos.map((obj)=>{
+
+      return ( <div className="todo">
+          <div className="left">
+
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
 
 
-class App  extends Component {
-  state = {
-    num:0
 
-    } 
-    increment=()=>{
-      this.setState({num:this.state.num+1});
+            }
 
-
-    }
-     decrement=()=>{
-      this.setState({num:this.state.num-1});
-
-
-    }
-
-
-  render() { 
-    
-
-    
-
-
-    return (
-    <div className='App'>
-    <div className='showcase'>
-    <h1>{this.state.num}</h1>
-    <button onClick={this.increment}>ADD</button>
-    <button  onClick={this.decrement}>SUB</button>
-
-
-
+            } value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
+          </div>
+          <div className="right">
+              <i onClick={(e)=>{
+                setToDos(toDos.filter(obj2 => {
+                  let temp;
+                  if (obj2.id != obj.id){
+                    temp = obj2
+                  }
+                  return temp;
+                }));
+              }} className="fas fa-times"></i>
+          </div>
+          
+        </div>)
+     
+      })
+      }
+      </div>
     </div>
- 
-
-
-
-
-    </div>);
-  }
+  );
 }
- 
-export default App ;
+
+export default App;
